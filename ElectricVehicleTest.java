1ce74ed8c9bec8c9b4e2bd61dda4054ec4a45e1e
Gonzalo <gcortesm@alumnos.unex.es>
2025-11-12 18:00:32 +0100

    a

--- ElectricVehicleTest.java
+++ ElectricVehicleTest.java
@@ -14,6 +14,9 @@ import org.junit.jupiter.api.Test;
 public class ElectricVehicleTest
 {
     private ElectricVehicle v1;
+    private EVCompany c;
+    private Location l;
+    private Location target;
     
     /**
      * Default constructor for test class ElectricVehicleTest
@@ -30,6 +33,11 @@ public class ElectricVehicleTest
     @BeforeEach
     public void setUp()
     {
+        c = new EVCompany("nueva");
+        l = new Location(5, 8);
+        v1 = new ElectricVehicle(c, l, new Location(20, 20), "name", "plate", 200);
+        target = new Location(20, 20);
+        c.addChargingStation(new ChargingStation("Cáceres", "1", target));
     }
 
     /**
@@ -44,17 +52,38 @@ public class ElectricVehicleTest
     
     @Test
     public void testCreation(){
-        EVCompany c =new EVCompany("nueva");
-        Location l = new Location(5, 8);
-        Location target = new Location(20, 20);
         v1=new ElectricVehicle(new EVCompany("nueva"), new Location(5, 8), new Location(20, 20), "name", "plate", 200);
-        assertTrue(c.equals(v1.getCompany()) && l.equals(v1.getLocation())
-        assertTrue()
-        && !v1.hasRechargingLocation() && v1.getTargetLocation().equals(target)
-        && v1.getName().equals("name") && v1.getPlate().equals("plate")
-        && v1.getBatteryCapacity()==200 && v1.getIdleCount()==0
-        && v1.getBatteryLevel()==200 && v1.getIdleCount()==0
-        && v1.getKwsCharged()==0 && v1.getChargesCount()==0
-        && v1.getChargestCost()==0);
+        EVCompany otra=new EVCompany("nueva");
+        otra.addChargingStation(new ChargingStation("Cáceres", "1", target));
+        
+        assertEquals(v1.getCompany(), otra);
+        assertEquals(v1.getLocation(), new Location(5, 8));
+        assertFalse(v1.hasRechargingLocation());
+        assertEquals(v1.getTargetLocation(), target);
+        assertEquals(v1.getName(), "name");
+        assertEquals(v1.getPlate(), "plate");
+        assertEquals(v1.getBatteryCapacity(), 200);
+        assertEquals(v1.getIdleCount(), 0);
+        assertEquals(v1.getBatteryLevel(), 200);
+        assertEquals(v1.getIdleCount(), 0);
+        assertEquals(v1.getKwsCharged(), 0);
+        assertEquals(v1.getChargesCount(), 0);
+        assertEquals(v1.getChargestCost(), 0);
+    }
+    
+    @Test
+    public void testEnoughBattery(){
+        assertTrue(v1.enoughBattery(v1.distanceToTheTargetLocation()));
+        v1.setTargetLocation(new Location(120, 120));
+        assertFalse(v1.enoughBattery(v1.distanceToTheTargetLocation()));
+    }
+    
+    @Test
+    public void testCalculateRoute(){
+        v1.calculateRoute();
+        assertEquals("5-8 -> 20-20", v1.getStringRoute());
+        v1.setTargetLocation(new Location(120, 120));
+        v1.calculateRoute();
+        assertEquals("5-8 -> 20-20 -> 120-120", v1.getStringRoute());
     }
 }
