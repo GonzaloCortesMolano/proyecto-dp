90c22cdd55d5265e65bce8c0435b62665f898a76
Gonzalo <gcortesm@alumnos.unex.es>
2025-12-10 18:21:02 +0100

    a

--- StandardEV.java
+++ StandardEV.java
@@ -1,3 +1,5 @@
+import java.util.List;
+import java.util.Iterator;
 
 /**
  * Write a description of class StandardEV here.
@@ -7,9 +9,6 @@
  */
 public class StandardEV extends ElectricVehicle
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
     /**
      * Constructor for objects of class StandardEV
      */
@@ -18,4 +17,43 @@ public class StandardEV extends ElectricVehicle
         super(company, location, targetLocation, name, plate, batteryCapacity);
         tipo=EnumVehicles.STANDARD;
     }
+    
+    public void calculateRechargingPosition()
+    {
+        List<ChargingStation> stations = this.getCompany().getCityStations();
+        Iterator<ChargingStation> it = stations.iterator();
+        int betterDistance = 999;
+        Location betterStation = null;
+
+        while (it.hasNext()) {
+            ChargingStation currentStation = it.next();
+            Location currentLocation = currentStation.getLocation();
+            int distToStation = this.getLocation().distance(currentLocation);
+            
+            if (enoughBattery(distToStation) && !getLocation().equals(currentLocation)) { //Si puedo llegar a la estación:
+                List<Charger> chargers=currentStation.getChargers();
+                Iterator<Charger> itCharger = chargers.iterator();
+                boolean enc=false;
+                //compruebas que tenga algun cargador compatible
+                while(itCharger.hasNext() && !enc){
+                    Charger currentCharger=itCharger.next();
+                    
+                    if(currentCharger.canCharge(this)){
+                        enc=true; //calculas la distancia si es compatible
+                        int distance = distToStation + currentLocation.distance(this.getTargetLocation());
+                        if (distance < betterDistance) { // Si es la actual la ruta más corta:
+                            betterStation = currentLocation;
+                            betterDistance = distance;
+                        }
+                    }
+                }
+                enc=false;
+            }   
+            setRechargingLocation(betterStation); // Si no se encuentra ninguna, se asigna null
+        }
+    }
+    
+    //recharge mirar tipo
+    //act el texto de cada paso
+    //equals comparar tipos
 }
