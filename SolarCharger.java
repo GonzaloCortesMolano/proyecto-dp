061e3ecd219e8a67d5e36994223a83459020b9e9
szambrani-create <szambrani@alumnos.unex.es>
2026-01-13 00:35:00 +0100

    Comentarios de todas las subclases de Charger.

--- SolarCharger.java
+++ SolarCharger.java
@@ -1,14 +1,25 @@
 
 /**
- * Write a description of class SolarCharger here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Represents a solar-powered charging unit that can recharge
+ * {@link ElectricVehicle} instances of tier {@link VehicleTier#VTC}.
+ * <p>
+ * This charger applies a discount to the base charging fee, simulating
+ * the use of renewable solar energy.
+ * </p>
+ *
+ * @author Ricardo Álvarez, Gonzalo Cortés y Sergio Zambrano
+ * @version 2026.13.1
  */
 public class SolarCharger extends Charger
 {
     /**
-     * Constructor for objects of class SolarCharger
+     * Constructs a {@code SolarCharger} with the given identifier, charging speed
+     * and charging fee.
+     *
+     * @param id    The unique identifier of the charger.
+     * @param speed The charging speed in kWh. Must be positive.
+     * @param fee   The base price per kWh. Must be non-negative.
+     * @throws IllegalArgumentException if speed is not positive or fee is negative.
      */
     public SolarCharger(String id, int speed, double fee)
     {
@@ -16,12 +27,31 @@ public class SolarCharger extends Charger
         types.add(VehicleTier.VTC);
     }
 
-    /*OVERRIDE*/
+    /**
+     * Calculates the total charging cost applying a solar discount.
+     * <p>
+     * The final price is 10% cheaper than the base fee calculated by
+     * {@link Charger#calculateFee(int)}.
+     * </p>
+     *
+     * @param kwsRecharging The number of kilowatt-hours recharged.
+     * @return The discounted charging cost.
+     */
     public double calculateFee(int kwsRecharging){
         //if(canCharge(vehicle)) (ya comprobamos esto dentro de recharge en Charger)
             return (super.calculateFee(kwsRecharging))*0.9;
     }
     
+    /**
+     * Indicates whether this charger is equal to another object.
+     * Two {@code SolarCharger} objects are considered equal if they
+     * are equal according to {@link Charger#equals(Object)} and are
+     * instances of the same class with identical supported vehicle types.
+     *
+     * @param obj The object to compare with.
+     * @return {@code true} if the given object represents an equivalent
+     *         {@code SolarCharger}, {@code false} otherwise.
+     */
     @Override
     public boolean equals(Object obj){
         if(super.equals(obj)) {
--- UltraFastCharger.java
+++ UltraFastCharger.java
@@ -1,16 +1,26 @@
 
 /**
- * Write a description of class UltraFastCharger here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Represents a high-performance charging unit designed for premium electric vehicles.
+ * <p>
+ * This charger can only recharge {@link ElectricVehicle} instances of tier
+ * {@link VehicleTier#PREMIUM}. It applies a surcharge to the standard charging
+ * fee, increasing the final cost by 10%.
+ * </p>
+ *
+ * @author Ricardo Álvarez, Gonzalo Cortés y Sergio Zambrano
+ * @version 2026.13.1
  */
 public class UltraFastCharger extends Charger
 {
     
     /**
-     * @override
-     * Constructor for objects of class StandardCharger
+     * Constructs an {@code UltraFastCharger} with the given identifier,
+     * charging speed and base charging fee.
+     *
+     * @param id    The unique identifier of the charger.
+     * @param speed The charging speed in kWh. Must be positive.
+     * @param fee   The base price per kWh. Must be non-negative.
+     * @throws IllegalArgumentException if speed is not positive or fee is negative.
      */
     public UltraFastCharger(String id, int speed, double fee)
     {
@@ -18,12 +28,32 @@ public class UltraFastCharger extends Charger
         types.add(VehicleTier.PREMIUM);
     }
     
-    
+    /**
+     * Calculates the total charging fee for a given amount of energy.
+     * <p>
+     * The fee is calculated using the base implementation in {@link Charger}
+     * and then increased by 10%. The final amount is rounded to two decimal places.
+     * </p>
+     *
+     * @param kwsRecharging The number of kilowatt-hours to recharge.
+     * @return The final charging cost including the surcharge.
+     */
+    @Override
     public double calculateFee(int kwsRecharging) {
         double fee = super.calculateFee(kwsRecharging) * 1.1;
         return Math.round(fee * 100.0)/100.0;
     }
     
+    /**
+     * Indicates whether this charger is equal to another object.
+     * Two {@code UltraFastCharger} objects are considered equal if they
+     * are equal according to {@link Charger#equals(Object)} and are
+     * instances of the same class with identical supported vehicle types.
+     *
+     * @param obj The object to compare with.
+     * @return {@code true} if the given object represents an equivalent
+     *         {@code UltraFastCharger}, {@code false} otherwise.
+     */
     @Override
     public boolean equals(Object obj){
         if(super.equals(obj)) {
@@ -35,12 +65,4 @@ public class UltraFastCharger extends Charger
         return false;
     }
     
-    /**
-     * TODO devuelve si el vehiculo puede cargar ahi
-    
-    @Override
-    public boolean canCharge(ElectricVehicle vehicle){
-        return compareType(vehicle.getType());
-    }
-    */
 }
